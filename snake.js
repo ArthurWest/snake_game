ad67f06a0d6476137b3737eacff0a05058fc1ac6
Arthur <37119842+ArthurWest@users.noreply.github.com>
2020-01-16 17:55:06 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,49 +1,56 @@
 const Snake = {
-	x: 25,
-	y: 2,
+	x: 0,
+	y: 0,
 	tail: [],
 	parts: 1,
 	directionX: 0,
 	directionY: 0,
 	food: null,
-	squareSize: Table.cellSize,
-	canvasSquareSize: 600,
 	update: function() {
 		this.move();
-		this.render()
+		this.render();
 	},
 	setDirection: function(x, y) {
-		console.log("direction")
-		this.directionX = x;
-		this.directionY = y;
+		if (this.parts>1 && this.x+x==this.tail[this.parts-2].x && this.y+y==this.tail[this.parts-2].y) {
+			//Don't move
+			//TODO Make this work when you cross boundaries!
+		} else {
+			console.log("changed")
+			this.directionX = x;
+			this.directionY = y;
+		}
 	},
 	move: function() {
-		const numberOfCellsMinusOne = this.canvasSquareSize/this.squareSize-1;
+		const cols = Table.grid.length-1;//Index of the last col
+		const rows = Table.grid[0].length-1;//Index of the last row
 		//Checking for boundaries
 		if (this.x == 0 && this.directionX == -1) {//Going left
-			this.x = numberOfCellsMinusOne
-		} else if (this.x == numberOfCellsMinusOne && this.directionX == 1) {//Going right
+			this.x = cols
+		} else if (this.x == cols && this.directionX == 1) {//Going right
 			this.x=0
 		} else if (this.y == 0 && this.directionY == -1) {//Going top
-			this.y = numberOfCellsMinusOne
-		} else if (this.y == numberOfCellsMinusOne && this.directionY == 1) {//Going bottom
+			this.y = rows
+		} else if (this.y == rows && this.directionY == 1) {//Going bottom
 			this.y = 0
 		} else {//Normal way
 			this.x += this.directionX
 			this.y += this.directionY
 		}
-		//Chekinf for collision
+		//Cheking for collision
 		if (Table.grid[this.x][this.y].snake == true) { //If in the cell we are moving is a part of snake, then length = 1
 			this.parts=1
 		}
+		//Moving, we are all set
+		Table.grid[this.x][this.y].snake = true //Add snake to the cell
+		this.tail.push(Table.grid[this.x][this.y])//Add this cell to the body
+		this.moved = true; // Has the snake moved since the last change of direction. It's for bug fixing
+
 		if (Table.grid[this.x][this.y].food == true) { //If in the cell we are moving is food, then length += 1
 			this.parts += 1
 			Table.grid[this.x][this.y].food = false
 			this.pickNewLocationForFood();
 		}
-		//Moving, we are all set
-		Table.grid[this.x][this.y].snake = true //Add snake to the cell
-		this.tail.push(Table.grid[this.x][this.y])//Add this cell to the body
+
 	},
 	render() {
 		let counter = 0;
@@ -59,19 +66,34 @@ const Snake = {
 			}
 		}
 		this.food.render();
-		// Table.grid[snakeObj.x][snakeObj.y].snake = true;
-		// Table.grid[snakeObj.x][snakeObj.y].render()
 	},
 	pickNewLocationForFood: function() {
-		const numberOfCellsMinusOne = this.canvasSquareSize/this.squareSize-1;
-		let x = Math.floor(Math.random() * numberOfCellsMinusOne)
-		let y = Math.floor(Math.random() * numberOfCellsMinusOne)
-		for (let i = 0; i < this.tail.length;i++) {
-			while (x == this.tail[i].x) {x = Math.floor(Math.random() * numberOfCellsMinusOne)}
-			while (y == this.tail[i].y) {y = Math.floor(Math.random() * numberOfCellsMinusOne)}
+		/** This function picks a new location for food where there is no snake
+		Returns nothing, just ends if there is no more space to choose from
+
+		*/
+		const options = []
+		for (let i = 0; i < Table.grid.length; i++) {
+			for (let j = 0; j < Table.grid[0].length;j++) {
+				if (Table.grid[i][j].snake == null) {
+					options.push(Table.grid[i][j])	
+				}
+			}
 		}
-		this.food = Table.grid[x][y]
-		this.food.food = true;
+		if (options.length < 1) {
+			console.log("There is no more space. Game has ended")
+			return;
+		}
+
+		const randomIndex = Math.floor(Math.random() * options.length);
+		let foodCell = options[randomIndex];
+
+		foodCell.food=true;
+		Snake.food = foodCell;
+	},
+	getStarted: function() {
+		this.tail[0] = Table.grid[this.x][this.y];
+		this.pickNewLocationForFood();
 	}
 }
 document.addEventListener("keydown", keyHandler);
